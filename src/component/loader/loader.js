92045b340cddd0a02ebefbcfc8ae88e32e79c438
ahmed-yar-khan <104447288+ahmed-yar-khan@users.noreply.github.com>
2024-11-22 13:50:07 +0500

    set navagition bar

--- src/component/loader/loader.js
+++ src/component/loader/loader.js
@@ -1,13 +1,13 @@
-import React from 'react'
+// import React from 'react'
 
-const loader = () => {
-  return (
-    // loader
-    <div id="loader">
-        <span className="color">Ahmed Yar</span>Khan 
-    </div>
-    // loader-end
-  )
-}
+// const loader = () => {
+//   return (
+//     // loader
+//     <div id="loader">
+//         <span className="color">Ahmed Yar</span>Khan 
+//     </div>
+//     // loader-end
+//   )
+// }
 
-export default loader
+// export default loader
